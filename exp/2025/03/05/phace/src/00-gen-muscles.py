40274768a815f8183c923dae16bb196de980c461
liblaf <30631553+liblaf@users.noreply.github.com>
2025-03-13 18:44:38 +0800

    refactor: update muscle generation logic and point data
    - replaces hardcoded muscle generation with a loop for scalability
    - updates `PICKED_POINTS_RAW` with new point data
    - reduces `Config.radius` from 10.0 to 8.0

--- exp/2025/03/05/phace/src/00-gen-muscles.py
+++ exp/2025/03/05/phace/src/00-gen-muscles.py
@@ -10,24 +10,33 @@ import liblaf.cherries as cherries  # noqa: PLR0402
 import liblaf.melon as melon  # noqa: PLR0402
 
 PICKED_POINTS_RAW: str = """
--16.224176 82.714378 30.822733
--29.040668 83.874290 65.429703
-22.384373 83.141403 32.309559
-32.414909 85.249756 70.671371
+-47.994461 87.488716 69.635818
+-31.954334 102.025208 31.058456
+-34.268002 84.964554 69.614693
+-15.076257 80.786217 28.818552
+33.227581 85.974991 72.720451
+24.210426 83.967018 31.085028
+48.894791 89.774902 71.670639
+32.459431 95.352417 29.826008
 """
+# -16.224176 82.714378 30.822733
+# -29.040668 83.874290 65.429703
+# 22.384373 83.141403 32.309559
+# 32.414909 85.249756 70.671371
 
 
 class Config(cherries.BaseConfig):
-    radius: float = 10.0
+    radius: float = 8.0
     raw: Path = Path("./data/00-raw/")
 
 
 @cherries.main()
 def main(cfg: Config) -> None:
     points: Float[np.ndarray, "N 3"] = np.loadtxt(io.StringIO(PICKED_POINTS_RAW))
-    muscles: list[pv.PolyData] = []
-    muscles.append(make_muscle(points[0:2], radius=cfg.radius))
-    muscles.append(make_muscle(points[2:4], radius=cfg.radius))
+    muscles: list[pv.PolyData] = [
+        make_muscle(points[muscle_id * 2 : muscle_id * 2 + 2], radius=cfg.radius)
+        for muscle_id in range(points.shape[0] // 2)
+    ]
     for muscle_id, muscle in enumerate(muscles):
         melon.save(cfg.raw / "muscles" / f"{muscle_id:02d}.vtp", muscle)
 
