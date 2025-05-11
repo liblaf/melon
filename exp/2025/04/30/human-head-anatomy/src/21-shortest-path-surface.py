dd428c11df62aff0db88a23b969ffaced49b7e9f
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-11 19:40:38 +0800

    chore(cherries): auto commit (on run end)
    🏃 View run 2025/04/30/human-head-anatomy at: https://mlflow.liblaf.me:38161/#/experiments/2/runs/447309fb6b1049aa835e66a6c60fc060
    🧪 View experiment 2 at: https://mlflow.liblaf.me:38161/#/experiments/2

--- exp/2025/04/30/human-head-anatomy/src/21-shortest-path-surface.py
+++ exp/2025/04/30/human-head-anatomy/src/21-shortest-path-surface.py
@@ -31,11 +31,15 @@ def main(cfg: Config) -> None:
     source: int
     target: int
     source, target = nearest_result["vertex_id"]
-    path: list[int] = nx.shortest_path(graph, source, target, weight="length")  # pyright: ignore[reportAssignmentType]
+    path: list[int] = nx.single_source_dijkstra(graph, source, target, weight="length")  # pyright: ignore[reportAssignmentType]
     ic(nx.path_weight(graph, path, weight="length"))
     path: Integer[np.ndarray, " P"] = np.asarray(path, dtype=int)
     surface.point_data["in-path"] = np.zeros((surface.n_points,), dtype=bool)
     surface.point_data["in-path"][path] = True
+
+    distance: dict[str, float] = nx.shortest_path_length(graph, source, weight="length")
+    surface.point_data["distance"] = np.zeros((surface.n_points,), dtype=float)
+    surface.point_data["distance"][list(distance.keys())] = list(distance.values())
 
     melon.save(cfg.output, surface)
     cherries.log_output(cfg.output)
