a89ea2839a8886e47d3939065f56bdb194ef0a6d
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-11 17:30:00 +0800

    chore(cherries): auto commit (on run end)
    🏃 View run 2025/04/30/human-head-anatomy at: https://mlflow.liblaf.me:38161/#/experiments/2/runs/76797d260ee34695a4008cf31a04895b
    🧪 View experiment 2 at: https://mlflow.liblaf.me:38161/#/experiments/2

--- exp/2025/04/30/human-head-anatomy/src/20-tetgen.py
+++ exp/2025/04/30/human-head-anatomy/src/20-tetgen.py
@@ -1,11 +1,9 @@
-import subprocess
-import tempfile
 from pathlib import Path
 
 import pyvista as pv
 
 import liblaf.melon as melon  # noqa: PLR0402
-from liblaf import cherries, grapes
+from liblaf import cherries
 
 
 class Config(cherries.BaseConfig):
@@ -24,31 +22,20 @@ def main(cfg: Config) -> None:
     cherries.log_input(cfg.skin)
     skin: pv.PolyData = melon.load_poly_data(cfg.skin)
 
-    with tempfile.TemporaryDirectory() as _tmpdir:
-        tmpdir = Path(_tmpdir)
-        cranium_file: Path = tmpdir / "cranium.ply"
-        mandible_file: Path = tmpdir / "mandible.ply"
-        skin_file: Path = tmpdir / "skin.ply"
-        melon.save(cranium_file, cranium)
-        melon.save(mandible_file, mandible)
-        melon.save(skin_file, skin)
-        csg: dict = {
+    tetmesh: pv.UnstructuredGrid = melon.tetwild(
+        {
             "operation": "difference",
-            "left": str(skin_file),
+            "left": skin,
             "right": {
                 "operation": "union",
-                "left": str(cranium_file),
-                "right": str(mandible_file),
+                "left": cranium,
+                "right": mandible,
             },
-        }
-        csg_file: Path = tmpdir / "csg.json"
-        grapes.save(csg_file, csg)
-        output_file: Path = tmpdir / "output.msh"
-        subprocess.run(
-            ["fTetWild", "--output", output_file, "--csg", csg_file], check=True
-        )
-        output: pv.UnstructuredGrid = melon.load_unstructured_grid(output_file)
-    melon.save(cfg.output, output)
+        },
+        csg=True,
+    )
+
+    melon.save(cfg.output, tetmesh)
     cherries.log_output(cfg.output)
 
 
