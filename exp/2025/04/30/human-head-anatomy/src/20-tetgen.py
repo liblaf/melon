de4ccc43f880c6bca377e97d8666cc7ad4d60882
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-13 11:05:50 +0800

    chore(cherries): auto commit (on run end)
    🏃 View run 2025/04/30/human-head-anatomy at: https://mlflow.liblaf.me:38161/#/experiments/2/runs/d141b8fc6ddb40e5b629f3710dc1cbde
    🧪 View experiment 2 at: https://mlflow.liblaf.me:38161/#/experiments/2

--- exp/2025/04/30/human-head-anatomy/src/20-tetgen.py
+++ exp/2025/04/30/human-head-anatomy/src/20-tetgen.py
@@ -32,8 +32,8 @@ def main(cfg: Config) -> None:
                 "right": mandible,
             },
         },
-        lr=0.05 * 0.2,
-        epsr=1e-3 * 0.2,
+        lr=0.05 * 0.3,
+        epsr=1e-3 * 0.3,
         csg=True,
     )
     cherries.log_metric("n_points", tetmesh.n_points)
