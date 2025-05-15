0b2a788c60c8c92f7d0c3f64025fe0a614c65c4b
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-15 17:04:44 +0800

    chore(cherries): auto commit (on run end)
    🏃 View run 2025/04/30/human-head-anatomy at: https://mlflow.liblaf.me:38161/#/experiments/2/runs/e45cda16bc404086828f0774f3df0588
    🧪 View experiment 2 at: https://mlflow.liblaf.me:38161/#/experiments/2

--- exp/2025/04/30/human-head-anatomy/src/33-muscle-fraction.py
+++ exp/2025/04/30/human-head-anatomy/src/33-muscle-fraction.py
@@ -1,12 +1,16 @@
+import concurrent
+import concurrent.futures
+import itertools
 from pathlib import Path
 
+import attrs
 import einops
 import numpy as np
 import pyvista as pv
 from jaxtyping import Bool, Float
 
 import liblaf.melon as melon  # noqa: PLR0402
-from liblaf import cherries, grapes
+from liblaf import cherries
 
 
 class Config(cherries.BaseConfig):
@@ -14,6 +18,8 @@ class Config(cherries.BaseConfig):
 
     full: Path = cherries.data("01-raw/Full human head anatomy.obj")
     tetgen: Path = cherries.data("02-intermediate/20-tetgen.vtu")
+
+    output: Path = cherries.data("02-intermediate/33-muscle-fraction.vtu")
 
 
 def main(cfg: Config) -> None:
@@ -30,32 +36,53 @@ def main(cfg: Config) -> None:
         ]
         muscle.field_data["muscle-direction"] = melon.as_trimesh(
             muscle
-        ).principal_inertia_vectors[:, 0]
+        ).principal_inertia_vectors[0]
         ic(muscle.field_data["muscle-direction"])
 
     tetmesh.cell_data["muscle-direction"] = np.zeros((tetmesh.n_cells, 3))
     tetmesh.cell_data["muscle-fraction"] = np.zeros((tetmesh.n_cells,))
-    for cid, cell in grapes.track(
-        enumerate(tetmesh.cell),
-        total=tetmesh.n_cells,
-        callback_stop=grapes.timing.callback.NOOP,
-    ):
-        cell: pv.Cell
-        barycentric: Float[np.ndarray, "N 3"] = melon.sample_barycentric_coords(
-            (cfg.n_samples, 4)
-        )
-        samples: Float[np.ndarray, "N 3"] = melon.barycentric_to_points(
-            einops.repeat(cell.points, "B D -> N B D", N=cfg.n_samples), barycentric
-        )
-        is_in: Bool[np.ndarray, " N"] = np.zeros((cfg.n_samples,), dtype=bool)
-        for muscle in muscles:
-            is_in |= melon.triangle.contains(muscle, samples)
-            tetmesh.cell_data["muscle-direction"][cid] = muscle.field_data[
-                "muscle-direction"
-            ]
-        tetmesh.cell_data["muscle-fraction"][cid] += (
-            np.count_nonzero(is_in) / cfg.n_samples
-        )
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for result in executor.map(
+            compute_muscle_fraction,
+            itertools.repeat(tetmesh),
+            itertools.repeat(muscles),
+            itertools.repeat(cfg.n_samples),
+            range(tetmesh.n_cells),
+        ):
+            tetmesh.cell_data["muscle-direction"][result.cid] = result.muscle_direction
+            tetmesh.cell_data["muscle-fraction"][result.cid] = result.muscle_fraction
+
+    melon.save(cfg.output, tetmesh)
+
+
+@attrs.frozen
+class Result:
+    cid: int
+    muscle_fraction: float
+    muscle_direction: Float[np.ndarray, " 3"]
+
+
+def compute_muscle_fraction(
+    tetmesh: pv.UnstructuredGrid, muscles: pv.MultiBlock, n_samples: int, cid: int
+) -> Result:
+    cell: pv.Cell = tetmesh.get_cell(cid)
+    barycentric: Float[np.ndarray, "N 3"] = melon.sample_barycentric_coords(
+        (n_samples, 4)
+    )
+    samples: Float[np.ndarray, "N 3"] = melon.barycentric_to_points(
+        einops.repeat(cell.points, "B D -> N B D", N=n_samples), barycentric
+    )
+    is_in: Bool[np.ndarray, " N"] = np.zeros((n_samples,), dtype=bool)
+    muscle_direction: Float[np.ndarray, " 3"] = np.zeros((3,))
+    muscle_fraction: float = 0.0
+    for muscle in muscles:
+        is_in |= melon.triangle.contains(muscle, samples)
+        muscle_direction = muscle.field_data["muscle-direction"]
+    muscle_fraction = np.count_nonzero(is_in) / n_samples
+
+    return Result(
+        cid=cid, muscle_fraction=muscle_fraction, muscle_direction=muscle_direction
+    )
 
 
 if __name__ == "__main__":
