abfa05fdc8f0573242f2221f3cf73e63cfc4259d
liblaf <30631553+liblaf@users.noreply.github.com>
2025-06-02 20:16:57 +0800

    chore(cherries): auto commit (on run end)
    🧪 View experiment 2025/04/30/human-head-anatomy/src/13-extract-components.py at: https://www.comet.com/liblaf/melon/a6f56534cfd344fc81c30866def62b66

--- exp/2025/04/30/human-head-anatomy/src/13-extract-components.py
+++ exp/2025/04/30/human-head-anatomy/src/13-extract-components.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import pydantic
 import pyvista as pv
 
 import liblaf.melon as melon  # noqa: PLR0402
@@ -8,28 +7,22 @@ from liblaf import cherries, grapes
 
 
 class Config(cherries.BaseConfig):
-    class Outputs(pydantic.BaseModel):
-        cranium: Path = cherries.data("02-intermediate/cranium.vtp")
-        mandible: Path = cherries.data("02-intermediate/mandible.vtp")
+    full: Path = cherries.input("01-raw/Full human head anatomy.obj")
+    groups: Path = cherries.input("02-intermediate/groups.toml")
 
-    full: Path = cherries.data("01-raw/Full human head anatomy.obj")
-    groups: Path = cherries.data("02-intermediate/groups.toml")
-    outputs: Outputs = Outputs()
+    cranium: Path = cherries.output("02-intermediate/cranium.vtp")
+    mandible: Path = cherries.output("02-intermediate/mandible.vtp")
 
 
 def main(cfg: Config) -> None:
-    cherries.log_input(cfg.full)
     full: pv.PolyData = melon.load_poly_data(cfg.full)
-    cherries.log_input(cfg.groups)
     groups: dict = grapes.load(cfg.groups)
 
     cranium: pv.PolyData = melon.triangle.extract_groups(full, groups["cranium"])
     mandible: pv.PolyData = melon.triangle.extract_groups(full, groups["mandible"])
 
-    melon.save(cfg.outputs.cranium, cranium)
-    cherries.log_output(cfg.outputs.cranium)
-    melon.save(cfg.outputs.mandible, mandible)
-    cherries.log_output(cfg.outputs.mandible)
+    melon.save(cfg.cranium, cranium)
+    melon.save(cfg.mandible, mandible)
 
 
 if __name__ == "__main__":
