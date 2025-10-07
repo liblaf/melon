88d465d816e3460918c5765c991718d22b3c0eb3
liblaf <30631553+liblaf@users.noreply.github.com>
2025-10-07 16:53:38 +0800

    chore(cherries): 2025-10-07T165118
    url: https://www.comet.com/liblaf/melon/e952470dc6184cae9300ad09f6131a3d
    exp_dir: exp/2025/04/30/human-head-anatomy
    cwd: exp/2025/04/30/human-head-anatomy
    cmd: python src/34-muscles.py
    params:
      n_samples: 100
    inputs:
    - exp/2025/04/30/human-head-anatomy/data/01-raw/Full human head anatomy.obj
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/groups.toml
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/23-tetgen.vtu
    outputs:
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/34-tetgen.vtu

--- exp/2025/04/30/human-head-anatomy/src/34-muscles.py
+++ exp/2025/04/30/human-head-anatomy/src/34-muscles.py
@@ -24,45 +24,6 @@ class Config(cherries.BaseConfig):
     output: Path = cherries.output("02-intermediate/34-tetgen.vtu")
 
 
-@attrs.define
-class TaskResult:
-    cid: int
-    muscle_fraction: float
-    muscle_orientation: Float[np.ndarray, "3 3"]
-
-
-def process_muscle(
-    tetmesh: pv.UnstructuredGrid, muscles: pv.MultiBlock, cid: int, n_samples: int
-) -> TaskResult:
-    cell: pv.Cell = tetmesh.get_cell(cid)
-    barycentric: Float[np.ndarray, "N 3"] = melon.sample_barycentric_coords(
-        (n_samples, 4)
-    )
-    samples: Float[np.ndarray, "N 3"] = melon.barycentric_to_points(
-        einops.repeat(cell.points, "B D -> n_samples B D", n_samples=n_samples),
-        barycentric,
-    )
-    is_in: Bool[np.ndarray, " N"] = np.zeros((n_samples,), dtype=bool)
-    muscle_orientation: Float[np.ndarray, "3 3"] = np.zeros((3, 3))
-    muscle_fraction: float = 0.0
-    major_muscle_fraction: float = 0.0
-    for muscle in muscles:
-        contains: Bool[np.ndarray, " N"] = melon.tri.contains(muscle, samples)
-        n_contains: int = np.count_nonzero(contains)  # pyright: ignore[reportAssignmentType]
-        if n_contains == 0:
-            continue
-        major_muscle_fraction = max(major_muscle_fraction, n_contains / n_samples)
-        is_in |= contains
-        muscle_orientation = np.reshape(
-            muscle.field_data["muscle-orientation"], shape=(3, 3)
-        )
-    muscle_fraction = np.count_nonzero(is_in) / n_samples  # pyright: ignore[reportAssignmentType]
-
-    return TaskResult(
-        cid=cid, muscle_fraction=muscle_fraction, muscle_orientation=muscle_orientation
-    )
-
-
 def main(cfg: Config) -> None:
     full: pv.PolyData = melon.load_polydata(cfg.full)
     tetmesh: pv.UnstructuredGrid = melon.load_unstructured_grid(cfg.tetgen)
@@ -82,6 +43,8 @@ def main(cfg: Config) -> None:
             fixed.user_dict["name"] = f"{group}.{block.point_data['RegionId'][0]}"
             ic(fixed.user_dict["name"])
             muscles.append(fixed)
+    muscle_names: list[str] = [m.user_dict["name"] for m in muscles]
+    muscle_name_to_id: dict[str, int] = {name: i for i, name in enumerate(muscle_names)}
 
     for muscle in muscles:
         muscle_tm: tm.Trimesh = melon.as_trimesh(muscle)
@@ -90,6 +53,7 @@ def main(cfg: Config) -> None:
         vectors = vectors[np.argsort(components)]
         muscle.field_data["muscle-orientation"] = vectors.ravel()
 
+    tetmesh.cell_data["muscle-id"] = np.full((tetmesh.n_cells,), -1, dtype=int)
     tetmesh.cell_data["muscle-fraction"] = np.zeros((tetmesh.n_cells,))
     tetmesh.cell_data["muscle-orientation"] = np.zeros((tetmesh.n_cells, 3, 3))
     with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -104,10 +68,17 @@ def main(cfg: Config) -> None:
             ),
             total=tetmesh.n_cells,
         ):
+            tetmesh.cell_data["muscle-id"][result.cid] = (
+                muscle_name_to_id[result.major_muscle_name]
+                if result.major_muscle_name is not None
+                else -1
+            )
             tetmesh.cell_data["muscle-fraction"][result.cid] = result.muscle_fraction
             tetmesh.cell_data["muscle-orientation"][result.cid] = (
                 result.muscle_orientation.ravel()
             )
+
+    tetmesh.field_data["muscle-name"] = muscle_names
     melon.save(cfg.output, tetmesh)
 
 
@@ -154,6 +125,52 @@ def compute_muscle_fraction(
         else None,
         muscle_direction=muscle_direction,
         muscle_fraction=muscle_fraction,
+    )
+
+
+@attrs.frozen(kw_only=True)
+class TaskResult:
+    cid: int
+    major_muscle_name: str | None
+    muscle_fraction: float
+    muscle_orientation: Float[np.ndarray, "3 3"]
+
+
+def process_muscle(
+    tetmesh: pv.UnstructuredGrid, muscles: pv.MultiBlock, cid: int, n_samples: int
+) -> TaskResult:
+    cell: pv.Cell = tetmesh.get_cell(cid)
+    barycentric: Float[np.ndarray, "N 3"] = melon.sample_barycentric_coords(
+        (n_samples, 4)
+    )
+    samples: Float[np.ndarray, "N 3"] = melon.barycentric_to_points(
+        einops.repeat(cell.points, "B D -> n_samples B D", n_samples=n_samples),
+        barycentric,
+    )
+    is_in: Bool[np.ndarray, " N"] = np.zeros((n_samples,), dtype=bool)
+    muscle_orientation: Float[np.ndarray, "3 3"] = np.zeros((3, 3))
+    muscle_fraction: float = 0.0
+    major_muscle_fraction: float = 0.0
+    major_muscle_name: str | None = None
+    for muscle in muscles:
+        contains: Bool[np.ndarray, " N"] = melon.tri.contains(muscle, samples)
+        n_contains: int = np.count_nonzero(contains)  # pyright: ignore[reportAssignmentType]
+        if n_contains == 0:
+            continue
+        if n_contains / n_samples > major_muscle_fraction:
+            major_muscle_fraction = n_contains / n_samples
+            major_muscle_name = muscle.user_dict["name"]
+        is_in |= contains
+        muscle_orientation = np.reshape(
+            muscle.field_data["muscle-orientation"], shape=(3, 3)
+        )
+    muscle_fraction = np.count_nonzero(is_in) / n_samples  # pyright: ignore[reportAssignmentType]
+
+    return TaskResult(
+        cid=cid,
+        major_muscle_name=major_muscle_name,
+        muscle_fraction=muscle_fraction,
+        muscle_orientation=muscle_orientation,
     )
 
 
