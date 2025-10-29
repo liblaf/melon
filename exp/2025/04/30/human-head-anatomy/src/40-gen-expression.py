89ba23713b7a4b07efab4e99d25723828f1ba51d
liblaf <30631553+liblaf@users.noreply.github.com>
2025-10-29 12:16:18 +0800

    chore(cherries): 2025/04/30/human-head-anatomy/src/40-gen-expression.py
    url: https://www.comet.com/liblaf/melon/744869eff1b14191877e25fe0023ee82
    exp_dir: exp/2025/04/30/human-head-anatomy
    cwd: exp/2025/04/30/human-head-anatomy
    cmd: python src/40-gen-expression.py
    inputs:
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/12-skin.vtp
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/12-skin.landmarks.json
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/14-cranium.ply
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/14-cranium.landmarks.json
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/14-mandible.ply
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/14-mandible.landmarks.json
    - exp/2025/04/30/human-head-anatomy/data/02-intermediate/34-tetgen.vtu
    - exp/2025/04/30/human-head-anatomy/data/40-expression-flame.vtp
    outputs:
    - exp/2025/04/30/human-head-anatomy/data/40-expression.vtu

--- exp/2025/04/30/human-head-anatomy/src/40-gen-expression.py
+++ exp/2025/04/30/human-head-anatomy/src/40-gen-expression.py
@@ -1,12 +1,48 @@
 from pathlib import Path
 
+import numpy as np
 import pyvista as pv
+from jaxtyping import Bool, Integer
 
 import liblaf.melon as melon  # noqa: PLR0402
 from liblaf import cherries
 
 
+def classify(
+    surface: pv.PolyData, include: pv.PolyData, exclude: pv.PolyData
+) -> Bool[np.ndarray, " N"]:
+    nearest_algo: melon.NearestAlgorithm = melon.NearestPointOnSurface(
+        distance_threshold=np.inf,
+        fallback_to_nearest_vertex=False,
+        normal_threshold=-np.inf,
+    )
+    nearest_include: melon.NearestResult = melon.nearest(
+        include, surface, algo=nearest_algo
+    )
+    nearest_exclude: melon.NearestResult = melon.nearest(
+        exclude, surface, algo=nearest_algo
+    )
+    is_include: Bool[np.ndarray, " N"] = (
+        nearest_include["distance"] <= nearest_exclude["distance"]
+    )
+    return is_include
+
+
+def transfer_trimesh_point_data_to_tetmesh(
+    tetmesh: pv.UnstructuredGrid, surface: pv.PolyData, data_name: str
+) -> pv.UnstructuredGrid:
+    point_ids: Integer[np.ndarray, " N"] = surface.point_data["point-id"]
+    data: Bool[np.ndarray, " N"] = np.zeros((tetmesh.n_points,), dtype=bool)
+    data[point_ids[surface.point_data[data_name]]] = True
+    tetmesh.point_data[data_name] = data
+    return tetmesh
+
+
 class Config(cherries.BaseConfig):
+    skin: Path = cherries.input("02-intermediate/12-skin.vtp")
+    cranium: Path = cherries.input("02-intermediate/14-cranium.ply")
+    mandible: Path = cherries.input("02-intermediate/14-mandible.ply")
+
     tetgen: Path = cherries.input("02-intermediate/34-tetgen.vtu")
     source: Path = cherries.input("40-expression-flame.vtp")
 
@@ -31,6 +67,35 @@ def main(cfg: Config) -> None:
         fill=0.0,
         # nearest=melon.proximity.NearestPoint(normal_threshold=0.0),
     )
+
+    skin: pv.PolyData = melon.load_polydata(cfg.skin)
+    exclude_groups: list[str] = [
+        "Ear",
+        "EarNeckBack",
+        "EarSocket EyeSocketTop",
+        "EyeSocketBottom",
+        "EyeSocketTop",
+        "HeadBack",
+        "LipInnerBottom",
+        "LipInnerTop",
+        "MouthSocketBottom",
+        "MouthSocketTop",
+        "NeckBack",
+        "NeckFront",
+        "Nostril",
+    ]
+    face: pv.PolyData = melon.tri.extract_groups(skin, exclude_groups, invert=True)
+    not_face: pv.PolyData = pv.merge(
+        [
+            melon.io.load_polydata(cfg.cranium),
+            melon.io.load_polydata(cfg.mandible),
+            melon.tri.extract_groups(skin, exclude_groups),
+        ]
+    )
+    surface.point_data["is-face"] = classify(surface, face, not_face)
+
+    tetmesh.point_data["is-skin"] = tetmesh.point_data["is-face"]
+    tetmesh = transfer_trimesh_point_data_to_tetmesh(tetmesh, surface, "is-face")
     melon.io.save(cfg.output, tetmesh)
 
 
