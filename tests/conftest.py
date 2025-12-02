73b6e35d0c67f4bec7464f3b94ecec6d0d1bd82c
liblaf <30631553+liblaf@users.noreply.github.com>
2025-12-03 01:30:03 +0800

    test(conftest): Configure Warp for test debug output
    Explicitly set Warp's debug, verbose, and verbose_warnings configurations to true during pytest setup to provide more detailed output for debugging and understanding Warp-related test failures or behavior.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,7 @@ import warp as wp
 
 
 def pytest_configure(config: pytest.Config) -> None:  # noqa: ARG001
+    wp.config.mode = "debug"
+    wp.config.verbose = True
+    wp.config.verbose_warnings = True
     wp.init()
